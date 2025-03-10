10c31b60635f596fb949b274fdf443e1471d5474
Ritesh Dhuware <riteshdhuware66@gmail.com>
2025-03-10 13:53:43 +0530

    Completed Food Partner side

--- food-delivery-backend/src/config/db.js
+++ food-delivery-backend/src/config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 
 const connectionDetails = {
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: 'W2_87274_Ritesh',
+    password: 'manager',
+    
     database: 'food_delivery_db',
-    port: 3307,
+    // port: 3307,
 };
 const conn = mysql.createPool(connectionDetails);
 module.exports = { conn };
